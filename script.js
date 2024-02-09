65c0a202cd048ee9adb85c69c1f0506aacc0e83a
Yash Sharma <139811734+YashSharma412@users.noreply.github.com>
2024-02-09 15:15:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function trailingZeros(n) {
   //your JS code here. If required.
+	n = Number(n)
+	if (n < 0) {
+		// n is negative
+		return -1;
+	}
+
+	let count = 0;
+	for (let i = 5; Math.floor(n/i) >= 1; i = i*5) {
+		count += Math.floor(n/i)
+	}
+	return count
 }
 
 const input = prompt("Enter a number");
